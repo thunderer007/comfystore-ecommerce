c0e5d7ae0c14d3a0fa6deed32e4bfea063f48e9c
Joshua Faniyi <faniyijoshua9@gmail.com>
2025-02-19 16:55:13 +0100

    made some changes to the hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+import hero1 from "../assets/hero1.webp";
+import hero2 from "../assets/hero2.webp";
+import hero3 from "../assets/hero3.webp";
+import hero4 from "../assets/hero4.webp";
+
+const Hero = () => {
+  return <div>Hero</div>;
+};
+
+export default Hero;
